ac6d77f17d1d6e43e1999b5344146e6342d70f0c
vincentpham123 <vincentpham32@gmail.com>
2023-11-20 17:37:52 -0800

    wrote handle submit for fetch post request

--- email-git-login/src/EmailGitForm.js
+++ email-git-login/src/EmailGitForm.js
@@ -7,19 +7,37 @@ const EmailGitForm = () => {
     const [repoValue, setRepoValue] = useState('');
 
     const [validEmail, setValidEmail] = useState(false);
-    
-
+    const [success,setSuccess] = useState('')
+    const [error, setErrors] = useState('')
     //handle submit 
     // create formData and submit to fetch as a post 
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
         const formData = new FormData();
         formData.append('email',emailValue);
         formData.append('githubRepoUrl', repoValue);
 
+        try{
+            let response =  await fetch('https://cv-devs-temp-challenge.vercel.app/api/challenge',
+                {
+                    method: 'POST',
+                    body: formData
+                }
+            )
 
+            if (response.ok){
+                const data = await response.json()
+                setSuccess(data)
+            } else{
+                const errorResponse = await response.json()
+                setErrors(error.error)
+            }
+        } catch (error){
+            setErrors('An error occured with the requeset')
+        }
+        
         // set emailValue and repoValue 
         // only allow submission 
     }
